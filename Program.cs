227d249d59c5af6d956f492b5988234fe39f5b44
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-10-19 10:17:58 -0400

    add HTTPClient to print image to badge

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 
 namespace CatWorx.BadgeMaker 
 {
@@ -36,11 +36,12 @@ namespace CatWorx.BadgeMaker
     
     
   // Main method that runs on application initiation
-    static void Main(string[] args)
+    async static Task Main(string[] args)
     {
       List<Employee> employees = GetEmployees();
       Util.PrintEmployees(employees);
       Util.MakeCSV(employees);
+      await Util.MakeBadges(employees);
     }
   }
 }
--- Util.cs
+++ Util.cs
@@ -1,5 +1,9 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Net.Http;
+using SkiaSharp;
+using System.Threading.Tasks;
 
 namespace CatWorx.BadgeMaker
 {
@@ -22,6 +26,55 @@ namespace CatWorx.BadgeMaker
         // If not, create it
         Directory.CreateDirectory("data");
       }
+      using (StreamWriter file = new StreamWriter("data/employees.csv"))
+      {
+        file.WriteLine("ID,Name,PhotoUrl");
+        // loop over the employees
+        foreach(var i in employees)
+        {
+          // write each employee to the file
+          string template = "{0}, {1}, {2}";
+          file.WriteLine(String.Format(template, i.GetId(), i.GetFullName(), i.GetPhotoUrl()));
+        }
+      }
+    }
+    async public static Task MakeBadges(List<Employee> employees)
+    {
+      // create the canvas that we'll use to make the badge
+      //LAYOUT VARIABLES
+      int BADGE_WIDTH = 669;
+      int BADGE_HEIGHT = 1044;
+
+      int PHOTO_LEFT_X = 184;
+      int PHOTO_TOP_Y = 215;
+      int PHOTO_RIGHT_X = 486;
+      int PHOTO_BOTTOM_Y = 517;
+
+
+      // create image. instance of HttpClient is disposed after code in black has run
+      using(HttpClient client = new HttpClient())
+      {
+        foreach(var i in employees)
+        {
+          SKImage photo = SKImage.FromEncodedData(await client.GetStreamAsync(i.GetPhotoUrl()));
+          SKImage background = SKImage.FromEncodedData(File.OpenRead("badge.png"));
+
+          SKBitmap badge = new SKBitmap(BADGE_WIDTH, BADGE_HEIGHT);
+          SKCanvas canvas = new SKCanvas(badge);
+
+          canvas.DrawImage(background, new SKRect(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+          canvas.DrawImage(photo, new SKRect(PHOTO_LEFT_X, PHOTO_TOP_Y, PHOTO_RIGHT_X, PHOTO_BOTTOM_Y));
+
+          SKImage finalImage = SKImage.FromBitmap(badge);
+          SKData data = finalImage.Encode();
+          data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
+
+          // how we'll see if the file was actually written
+          //SKData data = background.Encode();
+          //data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
+        }
+      }
+      
     }
   }
 }
